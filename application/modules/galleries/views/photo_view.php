3d91c2cee633d698850e3c9c0714057bcad92591
wuts <wuts73@gmail.com>
2009-11-05 21:54:47 +0800

    add photo details

--- application/modules/galleries/views/photo_view.php
+++ application/modules/galleries/views/photo_view.php
@@ -1,12 +1,14 @@
 <hr />
 
 <? // Show photos in this gallery ?>
-<div class="width-full">
+<div class="width-three-quaters">
 <? if(!empty($photo)): ?>
-	<ul class="photo">
-		<li><a href="<?= image_path('galleries/'.$photo->gallery_slug .'/' . $photo->filename); ?>" title="<?=$photo->description;?>" rel="modal"><?=image('galleries/' . $photo->gallery_slug . '/' . substr($photo->filename, 0, -4) . '_thumb' . substr($photo->filename, -4), '', array('title'=>$photo->description));?></a></li>
-		<li><?=$photo->description; ?></li>
-	</ul>		
+	<table>
+		<tr>
+                    <td><a href="<?= image_path('galleries/'.$photo->gallery_slug .'/' . $photo->filename); ?>" title="<?=$photo->description;?>" rel="modal"><?=image('galleries/' . $photo->gallery_slug . '/' . substr($photo->filename, 0, -4) . '_thumb' . substr($photo->filename, -4), '', array('title'=>$photo->description));?></a></td>
+		    <td valign="bottom"><?=$photo->description; ?></td>
+                </tr>
+	</table>
 <? else: ?>
 	<p><?=lang('gal_no_photo_error');?></p>
 <? endif; ?>
