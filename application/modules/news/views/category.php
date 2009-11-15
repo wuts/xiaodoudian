849dadf26b43501e35c3e94940befbf3b4744def
wuts <wuts73@gmail.com>
2009-11-15 10:50:25 +0800

    update news catagory

--- application/modules/news/views/category.php
+++ application/modules/news/views/category.php
@@ -1,5 +1,11 @@
-<h2>"<?=$category->title; ?>"<?=lang('news_articles_of_category_suffix');?></h2>
-<div class="float-left width-two-thirds">
+<script type="text/javascript">
+    $(document).ready(function(){       
+        $('#right-sidebar').corner("round 10px");
+        $('#middle-content').corner("round 10px");
+    ;})
+</script>
+
+<div id="middle-content"  class="float-left width-two-thirds">
 	<? if (!empty($news)): ?>	
 		<? foreach ($news as $article): ?>
 			<h3><?=anchor('news/'.date('Y/m', $article->created_on).'/'.$article->slug,$article->title);?></h3>			
@@ -13,8 +19,15 @@
 	<? endif; ?>
 </div>
 
-<div class="float-right width-quater">
-	<? $this->load->view('fragments/rss_box');?>	
-	<hr />	
-	<? $this->load->view('fragments/archive_box');?>	
+<div id="right-sidebar" class="float-right width-quater">
+	<h3><?=lang('news_catagories_label');?></h3>
+                 <div id="catagories">
+			<?php foreach ($this->news_m->getCategories() as $category): ?>
+                             <? if(!empty($category->category_title)): ?>
+			 <h4><?php echo anchor('news/category/'.$category->category_id,$category->category_title); ?></h4>
+                            <? endif;?>
+					  <!-- <p class="post-info">Posted in category : <?php echo anchor('news/category/'.$news->category_slug, $news->category_title);?></p> -->
+					  <!-- <p><?php echo strip_tags($news->intro); ?></p> -->
+			<?php endforeach ?>
+	</div>
 </div>
