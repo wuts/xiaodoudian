1861fde1dc5119ac55242bd6e9c0957cc54b6e5f
wuts <wuts73@gmail.com>
2009-11-24 19:52:35 +0800

    update free try link

--- application/themes/lazydays/views/header.php
+++ application/themes/lazydays/views/header.php
@@ -4,7 +4,7 @@
 
  <div class="float-right" style="padding:0.5em;text-align:right">
      <table>
-     <tr><td><?=image("icons/contact.gif","_theme_");?></td><td><a href="http://spreadsheets.google.com/viewform?formkey=dGdQUmZMZ1p6VzJ2VGxJWURnV05FcHc6MA"><span>13601834122</span></a></td><td><?=image("icons/calendar.gif");?></td><td><span style="color:black;"><a href="http://spreadsheets.google.com/viewform?formkey=dGdQUmZMZ1p6VzJ2VGxJWURnV05FcHc6MA">免费试听</a></span> | </td>
+     <tr><td><?=image("icons/contact.gif","_theme_");?></td><td><a href="https://spreadsheets.google.com/viewform?formkey=dGdQUmZMZ1p6VzJ2VGxJWURnV05FcHc6MA"><span>13601834122</span></a></td><td><?=image("icons/calendar.gif");?></td><td><span style="color:black;"><a href="http://spreadsheets.google.com/viewform?formkey=dGdQUmZMZ1p6VzJ2VGxJWURnV05FcHc6MA">免费试听</a></span> | </td>
          <td>
                          <? if($this->user_lib->logged_in()): ?>
 				<a href="<?=site_url('users/logout');?>"><?= lang('logout_label');?></a>
@@ -24,7 +24,7 @@
      <img src="<?=image_path('bg/header-right.gif', '_theme_') ?>" alt="right slice" class="balloons" />
      
      <span class="slogan">孩子天生就是创意大师，我们要做的只是帮助他们将这种天分发扬光大，而不要任其埋没在世俗的教条中！</span>
-     <div style="position:absolute !important;top:40px;right:30px;z-index:1;"><a href="http://spreadsheets.google.com/viewform?formkey=dGdQUmZMZ1p6VzJ2VGxJWURnV05FcHc6MA"><?=image("adv/free-try.png","",array('style'=>'width:200px;border:0;'));?></a></div>
+     <div style="position:absolute !important;top:40px;right:30px;z-index:1;"><a href="https://spreadsheets.google.com/viewform?formkey=dGdQUmZMZ1p6VzJ2VGxJWURnV05FcHc6MA"><?=image("adv/free-try.png","",array('style'=>'width:200px;border:0;'));?></a></div>
     
 </div>
 
